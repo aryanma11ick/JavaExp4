0ecfabc19f69bb20580005276458a09dcfe31ddf
Aryan mallick <161678352+aryanma11ick@users.noreply.github.com>
2025-03-11 09:20:19 +0530

    main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Deck deck = new Deck();
+
+        while (true) {
+            System.out.println("\nMenu");
+            System.out.println("1. Print Deck");
+            System.out.println("2. Print a Card");
+            System.out.println("3. Check if two cards are of the same suit");
+            System.out.println("4. Compare two cards");
+            System.out.println("5. Find a specific card");
+            System.out.println("6. Deal 5 random cards");
+            System.out.println("7. Shuffle Deck");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+
+        }
+    }
 }
+
